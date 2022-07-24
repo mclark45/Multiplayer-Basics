e898c9057c76c5fb9552f428cbf9da36d7c0bffb
mclark45 <80595429+mclark45@users.noreply.github.com>
2022-07-24 18:48:38 -0400

    added remote actions and server authority

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
+    #region Variables
+
     [SerializeField] private TMP_Text _displayNameText;
     [SerializeField] private Renderer _DisplayColorRenderer;
 
@@ -16,6 +18,10 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SyncVar(hook = nameof(HandleDisplayColorUpdated))]
     [SerializeField]
     private Color _displayColor = Color.black;
+
+    #endregion
+
+    #region Server
 
     [Server]
     public void SetDisplayName(string newDisplayName)
@@ -29,6 +35,22 @@ public class MyNetworkPlayer : NetworkBehaviour
         _displayColor = newDisplayColor;
     }
 
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        RpcLogName(newDisplayName);
+        SetDisplayName(newDisplayName);
+    }
+
+    [ClientRpc]
+    private void RpcLogName(string newName)
+    {
+        Debug.Log(newName);
+    }
+
+    #endregion
+
+    #region Client
     private void HandleDisplayNameUpdated(string oldName, string newName)
     {
         _displayNameText.text = newName;
@@ -38,4 +60,12 @@ public class MyNetworkPlayer : NetworkBehaviour
     {
         _DisplayColorRenderer.material.SetColor("_Color", newColor);
     }
+
+    [ContextMenu("Set My Name")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("My New Name");
+    }
+
+    #endregion
 }
